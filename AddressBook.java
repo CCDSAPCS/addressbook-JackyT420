e79e7f150314cf38a1940feea9de7becdb0866e1
JackyT420 <tereckj@countryday.net>
2017-03-16 09:40:16 -0400

    Update AddressBook.java

--- AddressBook.java
+++ AddressBook.java
@@ -10,7 +10,32 @@ public class AddressBook {
     public AddressBook() {
 		friends = new Contact[10];
 		numfriends = 0;
-    }
+	    
+ public void addContact(Contact c){
+	friend[numfriends]=c;
+		numfriends++;
+		}
+		
+		
+	public void printContacts90{
+		for(int i=0; i,numfriends; i++)
+			friends[i].printContact90;
+		}
+		
+	public int numContacts(){
+		return numfriends;
+		}
+		
+		
+	public int haveContacts(String s){
+		for (int i =0; i< numfriends; i++)
+		if (friends[i].getName().equals(s);{
+		    return i;}
+		    else {
+		    return -1;
+		    }
+    
+			    }
 public static void manu(){
 	systerm.out.println("1. add a contact");
 	systerm.out.println("2. delete a contact");
